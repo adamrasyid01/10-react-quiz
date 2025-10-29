5ee89d5926b521f9750e332f7f204899de656da8
Adam Rasyid N <adamrasyid01@gmail.com>
2025-10-29 08:58:55 +0700

    feat: Highscore Quiz

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
 
 const initialState = {
   questions: [],
@@ -15,6 +16,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -50,13 +52,19 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null,
       };
+    case "finish":
+      return {
+        ...state,
+        status: "finish",
+        highscore: state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Unknown action type: " + action.type);
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
@@ -88,9 +96,10 @@ export default function App() {
               answer={answer}
             />
             <Question question={questions[index]} dispatch={dispatch} answer={answer} />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton dispatch={dispatch} answer={answer} numQuestions={numQuestions} index={index} />
           </>
         )}
+        {status === "finish" && <FinishScreen points={points} maxPossiblePoints={maxPoints} highscore={highscore} />}
       </Main>
     </div>
   );
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🏆";
+  else if (percentage >= 80) emoji = "🎉";
+  else if (percentage >= 50) emoji = "😊";
+  else emoji = "😞";
+  return (
+    <>
+      <p className="result">
+        You Scored <strong>{points}</strong>out of {maxPossiblePoints} ({Math.ceil(percentage)}%) {emoji}
+      </p>
+      <p className="highscore">(Highscore : {highscore} points)</p>
+    </>
+  );
+}
